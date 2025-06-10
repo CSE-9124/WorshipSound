d33c78ec5ea930ac18c9507115b246f67dc5362e
Cholyn Sharon Enos <sharoncholyn@gmail.com>
2025-06-10 20:22:43 +0800

    chore: Setting build.gradle.kts untuk dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,26 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.recyclerview)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // OkHttp
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+
+    // Navigation Component
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+
+    // Picasso for image loading
+    implementation(libs.picasso)
+
+    // Glide for image loading
+    implementation(libs.glide)
+    implementation(libs.swiperefreshlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
